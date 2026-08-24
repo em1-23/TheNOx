ab9d4d7337727a6334a73fa85abfbaab4af66f11
m_ahmoud <melngar650@gmail.com>
2026-08-24 11:34:12 +0300

    Adding The Projects And Styling The Card

--- src/Pages/HomePages/ProjectsIdo.js
+++ src/Pages/HomePages/ProjectsIdo.js
@@ -1,4 +1,6 @@
 export const ProjectsIdo = [
   {id:1,Name:"Nox Profile Purpel One",ImageLink:"NoxProfile_Purpel",Link:"https://noxprofile.vercel.app/",Type:"React.js"},
-  {id:2,Name:"Clash Of Clans Game",ImageLink:"ClashOfClans",Link:"https://em1-23.github.io/Clash-Of-Clans/",Type:"Html"}
+  {id:2,Name:"Nox Profile Yellow One",ImageLink:"Profile_Yellow",Link:"https://github.com/em1-23/Portofilio",Type:"React.js"},
+  {id:3,Name:"Nox Gym Yellow One",ImageLink:"NOXGym",Link:"https://github.com/em1-23/Gym-NOX-Projects",Type:"React.js"},
+  {id:4,Name:"Clash Of Clans Game",ImageLink:"ClashOfClans",Link:"https://em1-23.github.io/Clash-Of-Clans/",Type:"Html"}
 ]
