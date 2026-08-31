c5c455a72916c37e0845d94c1f46ab3fe488380a
m_ahmoud <melngar650@gmail.com>
2026-08-31 09:55:31 +0300

    Adding Some For The Secound Page Projects And Experiments And Adding The Data For It -- Not Ending Yet

--- src/Pages/Pages2/Experiments.jsx
+++ src/Pages/Pages2/Experiments.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Exp } from './data for Exp/exp'
 
 function Experiments() {
   let Time = new Date()
@@ -10,12 +11,25 @@ function Experiments() {
   function ExpTemplate(Nox){
     let FirstTime = Nox.Time
     let AllTime = NewYear - Number(FirstTime)
-
+    if (AllTime === 0){
+      AllTime = "Learning Now"
+    }
+    else{
+      AllTime = `${AllTime} Year`
+    }
+    let Links = `/public/SVGS/${Nox.Linkss}.svg`
     return(
       <div className="ExpTemplate">
+        <img src={Links} alt={Nox.Name} />
         <h2 className="Name">{Nox.Name}</h2>
-        <span className="Exptime">Experiaments : {AllTime} Years</span> <br />
-        <a href="" className="YoutubeLink">▶ Youtube Link</a>
+        <span className="Exptime">Experiaments : {AllTime}</span> <br />
+      </div>
+    )
+  }
+  function RepTemplate(){
+    return(
+      <div className="RepoTemplate">
+        <div className="Name"></div>
       </div>
     )
   }
@@ -26,7 +40,16 @@ function Experiments() {
         <div className="Time">Date : {NewDay} / {NewMonth} / {NewYear}</div>
         <Link to="/"><h1 className='LogoName'>{"</>"} NoxCode</h1></Link>
       </div>
-      <ExpTemplate Name="Html & Css" Time="2024" />
+      <h1 className="Name">Skills</h1>
+      <div className="TemplatesSection">
+        {Exp.map((S)=>(
+          <ExpTemplate Name={S.Name} Time={S.Year} Linkss={S.Linkss} />
+        ))}
+      </div>
+      <h1 className='Name'>Projects</h1>
+      <div className="TemplateSection">
+
+      </div>
     </div>
   )
 }
--- src/Pages/Pages2/data for Exp/exp.js
+++ src/Pages/Pages2/data for Exp/exp.js
@@ -1,3 +1,8 @@
 export const Exp = [
-  {id:1,Year:2024,Skill:"Html & Css",YoutubeLink:""}
+  {id:1,Year:2024,Name:"Html & Css",Linkss:"Css"},
+  {id:2,Year:2025,Name:"JavaScript",Linkss:"JavaScript"},
+  {id:3,Year:2025,Name:"React.Js Vanilla",Linkss:"react"},
+  {id:4,Year:2025,Name:"React.Js Hooks",Linkss:"react"},
+  {id:5,Year:2026,Name:"Next.Js",Linkss:"Next"},
+  {id:6,Year:2026,Name:"Express.Js",Linkss:"Express"},
 ]
