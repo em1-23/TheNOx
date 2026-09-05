404650efb0655e04c7143f0f7589f9c18c6164cc
m_ahmoud <melngar650@gmail.com>
2026-09-05 03:20:07 +0300

    Adding The Flip Me Card For The Goals Terms

--- src/Pages/Pages2/Goals.jsx
+++ src/Pages/Pages2/Goals.jsx
@@ -12,23 +12,35 @@ function Goals() {
         <Link to="/"><h1 className='LogoName'>{"</>"} NoxCode</h1></Link>
       </div>
       <div className="TheTerms">
-        <div className="ShortTerm TermStyle">
-          <h1>Short Term</h1>
-          {FileListOne.map((e)=>(
-            <li className="ListStyle" key={e.id}>{e.id}. {e.List}</li>
-          ))}
+        <div className="TermStyleFlipMe" style={{'--ColorOfTheBackground':"#690080"}}>
+          <div className="Front">
+            <h1>Short Term</h1>
+          </div>
+          <div className="Back">
+            {FileListOne.map((e)=>(
+              <li className="ListStyle" key={e.id}>{e.id}. {e.List}</li>
+            ))}
+          </div>
         </div>
-        <div className="MidTerm TermStyle" style={{borderColor:"#0e8767"}}>
-          <h1>Mid Term</h1>
-          {FileListTwo.map((e)=>(
-            <li className="ListStyle" key={e.id}>{e.id}. {e.List}</li>
-          ))}
+        <div className="TermStyleFlipMe" style={{'--ColorOfTheBackground':"#0e8767"}}>
+          <div className="Front">
+            <h1>Mid Term</h1>
+          </div>
+          <div className="Back">
+            {FileListTwo.map((e)=>(
+              <li className="ListStyle" key={e.id}>{e.id}. {e.List}</li>
+            ))}
+          </div>
         </div>
-        <div className="LongTerm TermStyle" style={{borderColor:"#0f4d72"}}>
-          <h1>Long Term</h1>
-          {FileListThree.map((e)=>(
-            <li className="ListStyle" key={e.id}>{e.id}. {e.List}</li>
-          ))}
+        <div className="TermStyleFlipMe" style={{'--ColorOfTheBackground':"#0e3887"}}>
+          <div className="Front">
+            <h1>Long Term</h1>
+          </div>
+          <div className="Back">
+            {FileListThree.map((e)=>(
+              <li className="ListStyle" key={e.id}>{e.id}. {e.List}</li>
+            ))}
+          </div>
         </div>
       </div>
     </div>
